c142b13a7f214126be3be33b94fa9019c6733b57
Chris Done <chrisdone@gmail.com>
2016-12-03 22:03:52 +0000

    Limit 100

--- src/Hpaste/Model/Spam.hs
+++ src/Hpaste/Model/Spam.hs
@@ -39,7 +39,7 @@ generateSpamDB = do
       [ "SELECT title, content, author"
       , "FROM paste"
       , "WHERE NOT flaggedspam"
-      , "LIMIT 10000"
+      , "LIMIT 100"
       ]
       ()
   bad :: [(ByteString, ByteString, ByteString)] <-
@@ -47,7 +47,7 @@ generateSpamDB = do
       [ "SELECT title, content, author"
       , "FROM paste"
       , "WHERE flaggedspam"
-      , "LIMIT 10000"
+      , "LIMIT 100"
       ]
       ()
   liftIO
